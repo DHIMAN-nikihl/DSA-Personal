a63523bd1543ee3e3fd386f3c524d48c24d356fe
DHIMAN-nikihl <nikhilyt2004@gmail.com>
2026-07-12 15:48:58 +0530

    Time: 3 ms (91.79%), Space: 14.9 MB (96.34%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,61 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    void insertCopyInBetween(Node* head){
+        Node* temp = head;
+        while(temp!=nullptr){
+            Node* copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = temp->next->next;
+        }
+    }
+    void connetRandomPointer(Node* head){
+        Node* temp = head;
+        while(temp!=nullptr){
+            Node* copyNode = temp->next;
+            if(temp->random){
+                copyNode->random=temp->random->next;
+            }
+            else{
+                copyNode->random = nullptr;
+            }
+            temp = temp->next->next;
+        }
+    }
+
+    Node* copyRandomPointerList(Node* head){
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+        Node* temp = head;
+
+        while(temp!=nullptr){
+            res->next = temp->next;
+            res = res->next;
+            temp->next = temp->next->next;
+            temp = temp->next;
+        }
+        return dummyNode->next;
+    }
+
+    Node* copyRandomList(Node* head) {
+        insertCopyInBetween(head);
+        connetRandomPointer(head);
+        return copyRandomPointerList(head);
+    }
+};
